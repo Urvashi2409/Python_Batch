efeba7af3507eb7c1084725de937ed7eeda4265b
Urvashi2409 <urvashicode2410@gmail.com>
2023-07-24 15:13:18 +0530

    python basics

---
+++ test.py
@@ -0,0 +1,116 @@
+# print("Hello world!")
+
+# print statement is something which can be used to display on screen 
+
+# data types 
+
+# naming conventions of varible 
+
+# ray mond 
+# case sensitive 
+# cannot start with number 
+# no keyword 
+# symbols are not allowed 
+# _ is allowed 
+
+# arithematic operators = +, -, *, /, %, **, // 
+
+# print(3**2)
+# print(8/3)
+# print(8//2)
+
+# assignment operators 
+# =, +=, 
+# x = 23
+
+# x = 2;
+# x += 3
+# x = x + 3
+# print(x)
+
+# x -= 3
+
+# x *= 3
+
+# x /= 3
+
+# x %= 3
+
+# x //=3
+
+# x **= 3
+
+# relational operators  
+# <, >, !=, ==, >=, <= 
+# conditional statements 
+# age = 18 
+# if age < 18: 
+#     print("You are not eligible to vote")
+# elif age > 18:
+#     print("You are eligible to vote")
+# else:
+#     print("I am going to apply for voter id")
+
+
+# logical operators 
+
+# if: 
+
+# if : 
+
+# if: 
+
+# if: 
+
+# elif: 
+
+# elif: 
+
+# else: 
+# | 
+
+# x = 12
+# y = 3
+# z = 11
+# if (x == 2 and x < 3):
+#     print(x)
+# if x != 5:
+#     print("whatever")
+# if x != 5 and y >= 5 and z <= 13:
+#     print("its gonna happen")
+# if z != 0 or x == 2:
+#     print("you say")
+# if (not(y < 10)):
+#     print(y)
+# elif(x < 10 or x < 5):
+#     print("okkkkkkk")
+# elif(y < 10 or y <= 0):
+#     print("pikachu")
+# elif(z == 0 or y ==5):
+#     print("Pikaboo")
+# else:
+#     print("Default value")
+
+
+# Data types: 
+# int = integer 
+# float = decimal point numbers ex: 8.4567 
+# double = decimal point number ex: 23.443746482384648234783284362384632843874436832
+# complex = :  x = 1j 
+
+# x = 23
+
+# print(type(1j))
+# print(type(23))
+# print(type(23.45))
+# print(type(23.45227462874628428424624412462147816418424821642146242428734434623463248267846))
+# print(type("Apurv hiiii"))
+# print(type(True))
+
+# mylist = [2, 3, 4, 5, 'person', "apurv", '''urvashi''', True, 45.6]
+# print(len(mylist))
+# print(mylist)
+
+# print(mylist[5])
+
+# print(mylist[-4])
